58e2b451cbddfa48ca522982ecd5827c65f454db
adzhigitovruslan <adzhigitovruslan@gmail.com>
2023-04-17 18:34:38 +0800

    feat: add solution to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -10,13 +10,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
+ * transform( ) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!"); 
+  }
+  let newArr = [...arr];
+  newArr.reduce((total, curr, idx, array) => {
+  	switch(curr) {
+    	case '--discard-next':
+      	array[idx+1] ? array.splice(idx, 2, 0) : array.splice(idx, 1, 0);
+      break;
+      case '--discard-prev':
+      	array[idx-1] ? array.splice(idx-1, 2, 0) : array.splice(idx, 1, 0);
+      break;
+      case '--double-next':
+      	array[idx+1] ? array.splice(idx, 1, array[idx+1]) : array.splice(idx, 1, 0);
+      break;
+      case '--double-prev':
+        array[idx-1] ? array.splice(idx, 1, array[idx-1]) : array.splice(idx, 1, 0);
+      break;
+    }
+    return array
+  }, [])
+  return newArr.filter(el => el !== 0);
 }
+
 
 module.exports = {
   transform
